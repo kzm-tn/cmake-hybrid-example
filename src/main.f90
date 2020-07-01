44d546b38c6cd67480723156f44d81daa359034d
Tani_macbookpro2016 <tani@aero.t.u-tokyo.ac.jp>
2020-07-01 22:04:28 +0900

    mpi version

--- src/main.f90
+++ src/main.f90
@@ -1,4 +1,13 @@
 program main
       use c
-      print *, "hello world!"
+      implicit none
+      include "mpif.h"
+      integer :: nprocs
+      integer :: myrank
+      integer :: ierr
+      call mpi_init(ierr)
+      call mpi_comm_size(MPI_COMM_WORLD, nprocs, ierr)
+      call mpi_comm_rank(MPI_COMM_WORLD, myrank, ierr)
+      print *, "I am", myrank, "/", nprocs
+      call mpi_finalize(ierr)
 end program main
